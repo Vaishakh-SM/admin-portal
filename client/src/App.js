cd821fbe5193a651c0bfeb9d52934e233372d187
NCTzen2471 <66426509+NCTzen2471@users.noreply.github.com>
2021-11-29 23:54:05 +0530

    Update Landscaping Related

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,23 @@ import StoreProfile from "./Components/MarketShop/StoreProfile";
 import SubmitBill from "./Components/MarketShop/SubmitBill";
 import Register from "./Components/Register";
 import Feedback from "./Components/GeneralUser/Feedback";
+import LSAdd from "./Components/Landscaping/LSAdd";
+import LSLocation from "./Components/Landscaping/LSLocation";
+import LSCut from "./Components/Landscaping/LSCutRequest";
+import LSNotes from "./Components/Landscaping/LSNotes";
+import LSRead from "./Components/Landscaping/LSRead";
+import VNProfile from "./Components/Landscaping/VNProfile";
+import VNUpdate from "./Components/Landscaping/VNUpdate";
+import VNPays from "./Components/Landscaping/VNPays";
+import VNRepair from "./Components/Landscaping/VNRepair";
+import SSProfile from "./Components/Landscaping/SSProfile";
+import SSUpdate from "./Components/Landscaping/SSUpdate";
+import SSPayView from "./Components/Landscaping/SSPayView";
+import SSPayClear from "./Components/Landscaping/SSPayClear";
+import SSViewRecs from "./Components/Landscaping/SSViewRecs";
+import LSSchedule from "./Components/Landscaping/LSSchedule";
+import SSScheduler from "./Components/Landscaping/SSScheduler";
+import SSManual from "./Components/Landscaping/SSManual";
 
 const axios = require("axios");
 axios.defaults.withCredentials = true;
@@ -47,6 +64,23 @@ function App() {
         <Route path="/market/license_extension" element={<LicenseExt />} />
         <Route path="/general/profile" element={<GUProfile />} />
         <Route path="/general/feedback" element={<Feedback />} />
+        <Route path="/landscaping/add_details" element={<LSAdd />}/>
+        <Route path="/landscaping/update_loc" element={<LSLocation />}/>
+        <Route path="/landscaping/request_cut" element={<LSCut />}/>
+        <Route path="/landscaping/garden_note_add" element={<LSNotes />}/>
+        <Route path="/landscaping/garden_note" element={<LSRead />}/>
+        <Route path="/landscaping/vendor_profile" element={<VNProfile />}/>
+        <Route path="/landscaping/vendor_update" element={<VNUpdate />}/>
+        <Route path="/landscaping/vendor_pay" element={<VNPays />}/>
+        <Route path="/landscaping/repair_notes" element={<VNRepair />}/>
+        <Route path="/landscaping/supervisor_profile" element={<SSProfile />}/>
+        <Route path="/landscaping/supervisor_update" element={<SSUpdate />}/>
+        <Route path="/landscaping/super_vendor_pay" element={<SSPayView />}/>
+        <Route path="/landscaping/vendor_clear" element={<SSPayClear />}/>
+        <Route path="/landscaping/super_cut_recs" element={<SSViewRecs />}/>
+        <Route path="/landscaping/schedule" element={<LSSchedule />}/>
+        <Route path="/landscaping/scheduler" element={<SSScheduler />}/>
+        <Route path="/landscaping/manual_schedule" element={<SSManual />}/>
       </Routes>
     </Grommet>
   );
